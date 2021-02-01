634620ee30193c8943a8155850cfba094f383dc4
Zain-ul-Abdin1417 <zulabdin21@gmail.com>
2021-02-01 14:59:45 +0500

    Initial Upload

---
+++ src/ClinicSystem/PatientRegistration.java
@@ -0,0 +1,11 @@
+package ClinicSystem;
+
+import javax.swing.*;
+
+public class PatientRegistration {
+    private String patientName, Father_name, husband_name, PatientAddress, patientPhoneNumber;
+    private boolean New = false, followUp = false;
+    private int age;
+
+    private JPanel mainPane;
+}
